7877c4d8944cc1f9b9fe92b2dcb06cef7c6e0ab7
Eric Lagergren <ericscottlagergren@gmail.com>
2017-01-05 16:13:31 -0800

    xsrf: move from methods to just functions

--- xsrf/xsrf.go
+++ xsrf/xsrf.go
@@ -10,40 +10,31 @@ import (
 	"github.com/sermodigital/crypto/tokens"
 )
 
-// Token is an XSRF token.
-type Token struct{ tokens.AuthToken }
-
-// NewToken generates a new Token.
-func NewToken() Token {
-	return Token{AuthToken: tokens.NewAuthToken()}
-}
-
-// Hash returns the SHAKE-256 hash of the token. Hash should be used when
-// storing the XSRF token on the backend and hash comparisons should use a
-// constant-time comparison function, like those found in crypto/subtle.
-func (t Token) Hash() []byte {
-	return crypto.Shake256(t.AuthToken[:])
-}
-
 // CookieName is the name of the cookie angular checks to determine whether
 // it should send an "X-XSRF-TOKEN" header.
 const CookieName = "XSRF-TOKEN"
 
+// Hash returns the SHAKE-256 hash of the token. Hash should be used when
+// storing the XSRF token on the backend and hash comparisons should use a
+// constant-time comparison function, like those found in crypto/subtle.
+func Hash(t tokens.AuthToken) []byte {
+	return crypto.Shake256(t[:])
+}
+
 // Cookie returns an http.Cookie with the Name and Value fields set.
-func (t Token) Cookie() *http.Cookie {
-	return &http.Cookie{Name: CookieName, Value: string(t.AuthToken[:])}
+func Cookie(t tokens.AuthToken) *http.Cookie {
+	return &http.Cookie{Name: CookieName, Value: string(t[:])}
 }
 
 // Retrieve fetches the Token from the http.Request, if it exists.
-func Retrieve(r *http.Request) (Token, error) {
-	var t Token
+func Retrieve(r *http.Request) (t tokens.AuthToken, err error) {
 	c, err := r.Cookie(CookieName)
 	if err != nil {
 		return t, err
 	}
-	if len(c.Value) != len(t.AuthToken) {
+	if len(c.Value) != len(t) {
 		return t, errors.New(CookieName + " cookie's value is an incorrect length")
 	}
-	copy(t.AuthToken[:], c.Value)
+	copy(t[:], c.Value)
 	return t, nil
 }
